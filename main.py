3d2008e55f9d64caa9d0b71f624f38e9a76fdb2d
Saeed Prince <saeedurahman760@gmial.com>
2025-01-12 14:54:31 -0800

    'yes'

--- main.py
+++ main.py
@@ -120,6 +120,7 @@ async def search_urdu(keyword: str):
     
     return {"Results": result}
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+import uvicorn
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
